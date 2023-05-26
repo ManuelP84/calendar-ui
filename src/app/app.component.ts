e24fa7063bcfb407c4449b05ffb37221207348a1
Manuel Pineda <pineda.manuel@gmail.com>
2023-05-26 11:27:42 -0500

    insert, delete and get tasks implementation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { TaskService } from './services/task.service';
+import { v4 as uuidv4 } from 'uuid';
+
+interface TaskModel {
+  id: string;
+  title: string;
+  description: string;
+}
+
+const defaultTask: TaskModel = {
+  id: '',
+  title: '',
+  description: '',
+};
 
 @Component({
   selector: 'app-root',
@@ -7,13 +20,36 @@ import { TaskService } from './services/task.service';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
+  public task: TaskModel = { ...defaultTask };
   constructor(private taskService: TaskService) {}
 
-  public tasks: any[] = []
+  public tasks: TaskModel[] = [];
+  public inputTitle: string = ''
+  public inputDescription: string = ''
 
   ngOnInit(): void {
-    this.taskService.getAllTasks().subscribe(response => {
+    this.taskService.getAllTasks().subscribe((response) => {
       this.tasks = response;
-    })
+    });
+  }
+
+  saveTask(): void {
+    // const newID = new Date().getTime().toString();
+    this.task = {
+      id: uuidv4(),
+      title: this.inputTitle,
+      description: this.inputDescription,
+    };
+    this.taskService.insertTask(this.task).subscribe((resp) => {
+      if(resp === "OK") this.tasks.push(this.task);
+      this.inputTitle= '';
+      this.inputDescription = '';
+    });
+  }
+
+  deleteTask(id: string): void {
+    this.taskService.deleteTask(id).subscribe((resp) => {
+      if(resp === "OK") this.tasks = this.tasks.filter((task) => task.id != id);      
+    });
   }
 }
--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -6,11 +6,27 @@ import { Observable } from 'rxjs';
   providedIn: 'root',
 })
 export class TaskService {
-  private TASK_API_URL = 'http://localhost:8080/task/all';
+  private BASE_TASK_URL = 'http://localhost:8080/task/';
+  private ALL_URL = 'all';
+  private INSERT_URL = 'create';
+  private DELETE_URL = 'delete/';
+  private UPDATE_URL = 'update';
 
   constructor(private http: HttpClient) {}
 
   public getAllTasks(): Observable<any> {
-    return this.http.get(this.TASK_API_URL);    
+    return this.http.get(this.BASE_TASK_URL + this.ALL_URL);    
+  }
+
+  public insertTask(task: any): Observable<any> {
+    return this.http.post(this.BASE_TASK_URL + this.INSERT_URL, task);    
+  }
+
+  public deleteTask(id: string): Observable<any> {
+    return this.http.delete(this.BASE_TASK_URL + this.DELETE_URL + id);    
+  }
+
+  public updateTask(task: any): Observable<any> {
+    return this.http.put(this.BASE_TASK_URL + this.UPDATE_URL, task);    
   }
 }
